fdcdaf1075b08106072843111fcb5a3156d81094
Pjai1 <ulti40@hotmail.com>
2017-09-03 01:07:10 +0200

    Webpack not working yet

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,22 @@ import Header from './Header';
 import Main from './Main';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        };
+    }
+
     render() {
+        const children = this.props.children;
         return(
             <div className="container-fluid">
                 <Header />
                 <Main />
                 <h1>Root Component</h1>
-                {this.props.children}
+                {children}
             </div>
         );
     }
